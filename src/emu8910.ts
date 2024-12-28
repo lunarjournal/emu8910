51d8b3c4cd9e331b629c6a3e74ee3ea927f3cc9c
Lunar Journal <md.node.0@gmail.com>
2024-12-28 11:36:04 +0200

    Update emu8910.ts

--- src/emu8910.ts
+++ src/emu8910.ts
@@ -1,6 +1,51 @@
-// Copyright (C)
-// AY8910 emulator.
-// Author: Dylan Muller.
+/**
+ * @file gsctool.c
+ * @brief Tiny AY8910 PSG Emulator - emu8910.ts
+ *
+ * +---------------------------------------+
+ * |   .-.         .-.         .-.         |
+ * |  /   \       /   \       /   \        |
+ * | /     \     /     \     /     \     / |
+ * |        \   /       \   /       \   /  |
+ * |         "_"         "_"         "_"   |
+ * |                                       |
+ * |  _   _   _ _  _   _   ___   ___ _  _  |
+ * | | | | | | | \| | /_\ | _ \ / __| || | |
+ * | | |_| |_| | .` |/ _ \|   /_\__ \ __ | |
+ * | |____\___/|_|\_/_/ \_\_|_(_)___/_||_| |
+ * |                                       |
+ * |                                       |
+ * | Lunar RF Labs                         |
+ * | https://lunar.sh                      |
+ * |                                       |
+ * | RF Research Laboratories              |
+ * | Copyright (C) 2022-2024               |
+ * |                                       |
+ * +---------------------------------------+
+ *
+ * Copyright (c) 2022 Lunar RF Labs
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 const YM_CLOCK_ZX = 1750000;
 const FIR = [-0.011368,
